c2f067f022170c6dc1e4ad22d39e6c269f03f9cf
Jayash Yadav <jayash.yadav@geminisolutions.com>
2024-02-25 14:36:44 +0530

    node mailer

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config({ path: '.env' });
+const nodemailer = require('nodemailer');
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Routes
+app.get('/', async (req, res) => {
+    res.send('Hello, World!');
+});
+app.post('/contact', async (req, res) => {
+    try {
+        const { name, emailId, subject, message } = req.body;
+
+        const transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: process.env.emailId,
+                pass: process.env.password
+            }
+        });
+
+        const mailOptions = {
+            from: process.env.emailId,
+            to: 'jayash.work@gmail.com',
+            subject: subject,
+            text: `New Contact Form Submission\n\nName: ${name}\nEmail: ${emailId}\nMessage: ${message}` // Body of the email
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error(error);
+                res.status(500).send('Error: Failed to send email.');
+            } else {
+                console.log('Email sent: ' + info.response);
+                res.status(200).send('Email sent successfully!');
+            }
+        });
+    } catch (error) {
+        console.log("🚀 ~ file: index.js:22 ~ error:", error);
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
